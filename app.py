a9c8296db8b6bf705dddf5b3985933d7f3dd04a6
Adil Pathan <Adil.Pathan@lntinfotech.com>
2023-05-11 11:01:12 +0530

    added Azure connection

--- app.py
+++ app.py
@@ -19,49 +19,34 @@ def index():
 
 @app.route("/predict", methods=['GET', 'POST'])
 def predict():
-    input_data = list(request.form.values())
-    if int(input_data[0]) & int(input_data[3]) & input_data[2].isdigit() == True:
-        pass
+    from azure.devops.connection import Connection
+from msrest.authentication import BasicAuthentication
+import pprint
+
+# Fill in with your personal access token and org URL
+personal_access_token = 'gvmxrhhfgsvuuuurzto6mgbd4iwbvte4zajgt3sxp2izhqw2535q'
+organization_url = 'https://dev.azure.com/AdilPathan'
+
+# Create a connection to the org
+credentials = BasicAuthentication('', personal_access_token)
+connection = Connection(base_url=organization_url, creds=credentials)
+
+# Get a client (the "core" client provides access to projects, teams, etc)
+core_client = connection.clients.get_core_client()
+
+# Get the first page of projects
+get_projects_response = core_client.get_projects()
+index = 0
+while get_projects_response is not None:
+    for project in get_projects_response.value:
+        pprint.pprint("[" + str(index) + "] " + project.name)
+        index += 1
+    if get_projects_response.continuation_token is not None and get_projects_response.continuation_token != "":
+        # Get the next page of projects
+        get_projects_response = core_client.get_projects(continuation_token=get_projects_response.continuation_token)
     else:
-        print(ValueError)
-
-    if input_data[1] == 'female':
-        input_data[1] = 0
-    elif input_data[1] == 'male':
-        input_data[1] = 1
-    else:
-        print(ValueError)
-
-    if input_data[4] == 'no':
-        input_data[4] = 0
-    elif input_data[4] == 'yes':
-        input_data[4] = 1
-    else:
-        print(ValueError)
-
-    if input_data[5] == 'northeast':
-        input_data[5] = 0
-    elif input_data[5] == 'northwest':
-        input_data[5] = 1
-    elif input_data[5] == 'southeast':
-        input_data[5] = 2
-    elif input_data[5] == 'southwest':
-        input_data[5] = 3
-    else:
-        print(ValueError)
-
-    input_values = [x for x in input_data]
-    arr_val = [np.array(input_values)]
-    prediction = model.predict(arr_val)
-    output = round(prediction[0], 3)
-    return render_template('index.html', prediction_text=" The predicted insurance charges is {}".format(output),
-                           data=[{'gender': 'Gender'}, {'gender': 'female'}, {'gender': 'male'}],
-                           data1=[{'noc': 'Number of Children'}, {'noc': 0}, {'noc': 1}, {'noc': 2}, {'noc': 3},
-                                  {'noc': 4}, {'noc': 5}],
-                           data2=[{'smoke': 'Smoking Status'}, {'smoke': 'yes'}, {'smoke': 'no'}],
-                           data3=[{'region': "Region"}, {'region': "northeast"}, {'region': "northwest"},
-                                  {'region': 'southeast'}, {'region': "southwest"}])
-
+        # All projects have been retrieved
+        get_projects_response = None
 
 if __name__ == '__main__':
     app.run(debug=True)
